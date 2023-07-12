22a8b7f3c5f446c8f3bb6905a5526532bec72e71
ken <kimkhay8@gmail.com>
2023-07-12 16:35:20 +0800

    add Register and login link

---
+++ inc/footer.php
@@ -0,0 +1,64 @@
+<footer class="container">
+      <div class="whyKingz">
+        <h1>Why KINGZ CRYPTO HODL IS #1</h1>
+        <div class="kingz_descipt">
+          <div class="desc">
+            <img src="./assets/img/Vector.png" alt="" />
+            <div>
+              <p>Worldwide</p>
+              <small>Rapidly evolving and disruptive force in the global</small>
+            </div>
+          </div>
+          <div class="desc">
+            <img src="./assets/img/Group 1.png" alt="" />
+            <div>
+              <p>800k +</p>
+              <small>Active monthly user</small>
+            </div>
+          </div>
+          <div class="desc">
+            <img src="./assets/img/Group.png" alt="" />
+            <div>
+              <p>20k +</p>
+              <small>Active daily user</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <div class="text-center">
+      <p>List your favorite coin now!</p>
+      <p class="community">Get your community exposure</p>
+      <a href="#" target="_blank">LIST YOUR COIN</a>
+    </div>
+
+    <div class="sub-footer">
+      <img class="hide-sm" src="./assets/img/kingzhero.png" alt="" />
+      <ul>
+        <li>
+          <img src="./assets/img/Twitter.png" alt="" /><a
+            href="https://twitter.com/KingCryptoHodl"
+            target="_blank"
+            >Twitter</a
+          >
+        </li>
+        <li>
+          <img src="./assets/img/Discord.png" alt="" /><a
+            href="https://discord.gg/rKnJkWDMAg"
+            target="_blank"
+            >Discord</a
+          >
+        </li>
+        <li>
+          <img
+            style="margin-left: 15px"
+            src="./assets/img/Advertise.png"
+            alt=""
+          /><a href="https://twitter.com/KingCryptoHodl" target="_blank"
+            >Advertise</a
+          >
+        </li>
+      </ul>
+      <p class="footer-p">Copyright &copy; 2023 KINGZ CRYPTO HODL</p>
+    </div>
---
+++ inc/nav.php
@@ -0,0 +1,167 @@
+<header>
+      <div class="overlay hide-ovly"></div>
+      <section class="nav_menu hide-nav" id="menu">
+        <ul>
+          <li>
+            <p>Account</p>
+            <i class="fa-solid fa-xmark" id="close" style="color: #ffffff"></i>
+          </li>
+          <li>
+            <img src="./assets/img/login.png" alt="" />
+            <a href="http://" target="_blank" rel="noopener noreferrer"
+              ><p>Log in - (TBA)</p></a
+            >
+          </li>
+          <li>
+            <img src="./assets/img/watchlist.png" alt="" />
+            <a href="http://" target="_blank" rel="noopener noreferrer"
+              ><p>Watchlist - (TBA)</p></a
+            >
+          </li>
+          <li>
+            <img src="./assets/img/Sign Up.png" alt="" />
+            <a href="http://" target="_blank" rel="noopener noreferrer"
+              ><p>Sign up - (TBA)</p></a
+            >
+          </li>
+          <li><p>Cryptocurrency</p></li>
+          <li>
+            <img src="./assets/img/nwe_coin.png" alt="" />
+            <a href="http://" target="_blank" rel="noopener noreferrer"
+              ><p>New coin</p></a
+            >
+          </li>
+          <li>
+            <img src="./assets/img/Presale.png" alt="" />
+            <a href="http://" target="_blank" rel="noopener noreferrer"
+              ><p>Presales</p></a
+            >
+          </li>
+          <li>
+            <img src="./assets/img/Hot.png" alt="" />
+            <a href="http://" target="_blank" rel="noopener noreferrer"
+              ><p>Hot coins</p></a
+            >
+          </li>
+          <li><p>Giveaway</p></li>
+          <li>
+            <img src="./assets/img/Airdrop.png" alt="" />
+            <a href="http://" target="_blank" rel="noopener noreferrer"
+              ><p>Airdrops - (TBA)</p></a
+            >
+          </li>
+          <li><p>Create</p></li>
+          <li>
+            <img src="./assets/img/Submit.png" alt="" />
+            <a href="http://" target="_blank" rel="noopener noreferrer"
+              ><p>Submit coin</p></a
+            >
+          </li>
+          <li>
+            <img src="./assets/img/Update.png" alt="" />
+            <a href="http://" target="_blank" rel="noopener noreferrer"
+              ><p>Update coin</p></a
+            >
+          </li>
+          <li>
+            <img src="./assets/img/Contact Us.png" alt="" />
+            <a href="http://" target="_blank" rel="noopener noreferrer"
+              ><p>Contact Us</p></a
+            >
+          </li>
+          <li><p>News</p></li>
+          <li>
+            <img src="./assets/img/Blog.png" alt="" />
+            <a href="http://" target="_blank" rel="noopener noreferrer"
+              ><p>Blog</p></a
+            >
+          </li>
+          <li><p>Services</p></li>
+          <li>
+            <img src="./assets/img/nwe_coin.png" alt="" />
+            <a href="http://" target="_blank" rel="noopener noreferrer"
+              ><p>Advertising</p></a
+            >
+          </li>
+          <li>
+            <img src="./assets/img/Presale.png" alt="" />
+            <a href="http://" target="_blank" rel="noopener noreferrer"
+              ><p>Promote</p></a
+            >
+          </li>
+          <li>
+            <img src="./assets/img/Hot.png" alt="" />
+            <a href="http://" target="_blank" rel="noopener noreferrer"
+              ><p>Partnership</p></a
+            >
+          </li>
+        </ul>
+
+        <div class="follow_us">
+          <img src="./assets/img/Twitter.png" alt="" />
+          <a href="http://" target="_blank" rel="noopener noreferrer"
+            ><p>Follow us on Twitter</p></a
+          >
+        </div>
+      </section>
+      <style>
+        .topbar-right{
+          margin-left: auto;
+          order: 2;
+          padding-right: 250px;
+        }
+        a {
+          color: #00aa72;
+        }
+      
+
+      </style>
+      <div class="coins--list">
+      
+        <p>BNB: <span>$0</span></p>
+        <p>ETH: <span>$0</span></p>
+        <p>BTC: <span>$0</span></p>
+        <p>BNB: <span>$0</span></p>
+        <p>ETH: <span>$0</span></p>
+      
+        <div class="topbar-right is-hidden-mobile">
+          <a class="hlink" href="/register">Register</a> / <a  class="hlink" href="/login">Login</a>
+        </div>
+      </div>
+     
+      <section class="nav__header">
+        <nav class="nav__container container">
+          <div class="img__container">
+            <img src="./assets/img/logo_header.png" alt="KINGZ" width="50px" />
+            <img
+              src="./assets/img/kingzhero.png"
+              alt="kingzhero"
+              width="200px"
+              class="img__hero"
+            />
+          </div>
+          <div class="nav__item">
+            <div class="nav__items">
+              <input
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Search coins"
+                class="search-icon hide-sm"
+              />
+              <i class="fa-solid fa-magnifying-glass search-btn"></i>
+            </div>
+            <div class="menu__container">
+              <button class="btn__submit-coin" type="submit">
+                Submit coin
+              </button>
+              <div class="burger__menu">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </section>
+    </header>
